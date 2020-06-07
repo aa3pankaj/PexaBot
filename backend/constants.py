5a464bb55b0577eebb7e5526b515b388a62fa765
aa3pankaj <aa3pankaj@gmail.com>
2020-06-08 04:33:47 +0530

    updated front_end_url

--- backend/constants.py
+++ backend/constants.py
@@ -1,3 +1,3 @@
 DIALOGFLOW_LANGUAGE_CODE = 'en'
 exit_set = set(['bye', 'end', 'exit','Bye','Exit','End'])
-front_end_url = 'https://4cb0482b0f8b.ngrok.io/match/'
+front_end_url = 'https://pexabot.herokuapp.com/match/'
